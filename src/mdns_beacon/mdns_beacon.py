cd2d68b2e08cb50dcdf9a37068fa321212bc9803
Federico Jaureguialzo <fedejaure@gmail.com>
2020-10-18 20:26:30 +0200

    Add Beacon class

--- src/mdns_beacon/mdns_beacon.py
+++ src/mdns_beacon/mdns_beacon.py
@@ -1,1 +1,15 @@
 """Main module."""
+from typing import List, Optional
+
+from zeroconf import IPVersion
+
+
+class Beacon:
+    """mDNS Beacon."""
+
+    def __init__(
+        self, aliases: List[str], ip_version: IPVersion = IPVersion.All, ttl: Optional[int] = 60
+    ) -> None:
+        """Init a mDNS Beacon instance."""
+        self.aliases = aliases
+        self.ip_version = ip_version
