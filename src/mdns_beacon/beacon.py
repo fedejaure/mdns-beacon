cc5d403c57571235fb654c02ab49be456dd11f59
Federico Jaureguialzo <fedejaure@gmail.com>
2020-10-19 21:55:06 +0200

    Add beacon creation test

--- src/mdns_beacon/beacon.py
+++ src/mdns_beacon/beacon.py
@@ -8,8 +8,12 @@ class Beacon:
     """mDNS Beacon."""
 
     def __init__(
-        self, aliases: List[str], ip_version: IPVersion = IPVersion.All, ttl: Optional[int] = 60
+        self,
+        aliases: Optional[List[str]] = None,
+        ip_version: IPVersion = IPVersion.All,
+        ttl: int = 60,
     ) -> None:
         """Init a mDNS Beacon instance."""
-        self.aliases = aliases
+        self.aliases = aliases or []
         self.ip_version = ip_version
+        self.ttl = ttl
