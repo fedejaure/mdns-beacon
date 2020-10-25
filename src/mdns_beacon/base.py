88e5f2227a52b2a1a3336cacd57a0face6ecd57d
Federico Jaureguialzo <fedejaure@gmail.com>
2020-10-25 11:19:04 +0100

    Fix loop close

--- src/mdns_beacon/base.py
+++ src/mdns_beacon/base.py
@@ -38,5 +38,6 @@ class BaseBeacon(ABC):
         try:
             loop.run_forever()
         finally:
-            self.zeroconf.close()
+            loop.run_until_complete(loop.shutdown_asyncgens())
             loop.close()
+            self.zeroconf.close()
