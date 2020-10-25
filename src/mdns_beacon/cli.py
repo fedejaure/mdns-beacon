6e62599f34de7005e2e74fb7b490890fe4aae5af
Federico Jaureguialzo <fedejaure@gmail.com>
2020-10-25 12:54:06 +0100

    Add first version of the listen cmd

--- src/mdns_beacon/cli.py
+++ src/mdns_beacon/cli.py
@@ -1,16 +1,29 @@
 """Console script for mdns-beacon."""
 import click
+from zeroconf import ServiceStateChange, Zeroconf
 
 from mdns_beacon import __version__
+from mdns_beacon.listener import BeaconListener
 
 
-@click.command()
+def on_service_state_change(
+    zeroconf: Zeroconf, service_type: str, name: str, state_change: ServiceStateChange
+) -> None:
+    """On service state change handler."""
+    click.echo(f"Service {name} of type {service_type} state changed: {state_change}")
+
+
+@click.group()
 @click.version_option(version=__version__)
-def main() -> int:
-    """Console script for mdns-beacon."""
-    click.echo("Replace this message by putting your code into mdns_beacon.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
-    return 0
+def main() -> None:
+    """Simple multicast DNS (mDNS) client line interface utility."""  # noqa:  D401
+
+
+@main.command()
+def listen() -> None:
+    """Listen for services on the local network."""
+    listerner = BeaconListener(handlers=[on_service_state_change])
+    listerner.run_forever()
 
 
 if __name__ == "__main__":
