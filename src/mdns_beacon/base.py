3e7f4ed1bc017fa4065c71f5f6a1539375b4e2e0
Federico Jaureguialzo <fedejaure@gmail.com>
2020-10-25 09:20:05 +0100

    Add first attempt of base class

---
+++ src/mdns_beacon/base.py
@@ -0,0 +1,41 @@
+"""Base mDNS Beacon module."""
+import asyncio
+import logging
+from abc import ABC, abstractmethod
+
+from zeroconf import IPVersion, Zeroconf
+
+logger = logging.getLogger(__name__)
+
+
+class BaseBeacon(ABC):
+    """Base mDNS Beacon."""
+
+    _zeroconf: Zeroconf
+
+    def __init__(self, ip_version: IPVersion = IPVersion.All) -> None:
+        """Init a mDNS Beacon instance."""
+        self.ip_version = ip_version
+
+    @property
+    def zeroconf(self) -> Zeroconf:
+        """Zeroconf instance."""
+        if not self._zeroconf:
+            self._zeroconf = Zeroconf(ip_version=self.ip_version)
+        return self._zeroconf
+
+    @abstractmethod
+    def _execute(self) -> None:
+        """Execute Beacon work.
+
+        Method that derived beacons must override.
+        """
+
+    def run_forever(self) -> None:
+        """Run beacon."""
+        self._execute()
+        loop = asyncio.get_event_loop()
+        try:
+            loop.run_forever()
+        finally:
+            loop.close()
