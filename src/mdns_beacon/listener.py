aa01046385d7290fe3bdcab9081f5a1e0a0f1116
Federico Jaureguialzo <fedejaure@gmail.com>
2020-10-25 09:46:37 +0100

    Add first attempt of beacon listener

---
+++ src/mdns_beacon/listener.py
@@ -0,0 +1,28 @@
+"""mDNS listener module."""
+import logging
+from typing import Any, Callable, List, Optional, Union
+
+from zeroconf import ServiceBrowser, ServiceListener, ZeroconfServiceTypes
+
+from .base import BaseBeacon
+
+logger = logging.getLogger(__name__)
+
+
+class BeaconListener(BaseBeacon):
+    """mDNS Beacon listener."""
+
+    def __init__(
+        self,
+        handlers: Union[ServiceListener, List[Callable[..., None]]],
+        services: Optional[List[str]] = None,
+        *args: Any,
+        **kwargs: Any,
+    ) -> None:
+        """Init a mDNS Beacon listener."""
+        self.handlers = handlers
+        self.services = services or list(ZeroconfServiceTypes.find(zc=self.zeroconf))
+
+    def _execute(self) -> None:
+        """Listen for services on the local network."""
+        ServiceBrowser(zc=self.zeroconf, type_=self.services, handlers=self.handlers)
