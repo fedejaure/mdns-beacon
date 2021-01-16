8d89117d39984d3b19a63266111e25e50e8a69ba
Federico Jaureguialzo <fedejaure@gmail.com>
2021-01-16 13:08:45 +0100

    Fix listener tests

--- tests/test_listener.py
+++ tests/test_listener.py
@@ -4,7 +4,7 @@ import signal
 import threading
 import time
 from asyncio import AbstractEventLoop
-from typing import List, Optional, Union
+from typing import Any, Dict, Set
 
 import pytest
 from pytest_mock import MockerFixture
@@ -14,27 +14,49 @@ from mdns_beacon.listener import BeaconListener
 
 
 @pytest.mark.parametrize(
-    "ip_version,services,timeout",
+    "beacon_params,expected_services",
     [
-        (None, None, 2),
-        (IPVersion.V4Only, None, 2),
-        (IPVersion.V6Only, None, 2),
-        (None, ["_http._tcp.local."], 0.5),
-        (IPVersion.V4Only, ["_http._tcp.local."], 0.5),
-        (IPVersion.V6Only, ["_http._tcp.local."], 0.5),
+        ({"ip_version": None, "services": None, "timeout": 2}, BeaconListener._DEFAULT_SERVICES),
+        (
+            {"ip_version": IPVersion.V4Only, "services": None, "timeout": 2},
+            BeaconListener._DEFAULT_SERVICES,
+        ),
+        (
+            {"ip_version": IPVersion.V6Only, "services": None, "timeout": 2},
+            BeaconListener._DEFAULT_SERVICES,
+        ),
+        (
+            {
+                "ip_version": None,
+                "services": ["_http._tcp.local.", "_http._tcp.local."],
+                "timeout": 0.5,
+            },
+            {"_http._tcp.local."},
+        ),
+        (
+            {"ip_version": IPVersion.V4Only, "services": ["_hap._tcp.local."], "timeout": 0.5},
+            {"_hap._tcp.local."},
+        ),
+        (
+            {
+                "ip_version": IPVersion.V6Only,
+                "services": ["_some_type._some_protocol.local."],
+                "timeout": 1,
+            },
+            {"_some_type._some_protocol.local."},
+        ),
     ],
 )
 def test_beacon_listener(
     mocker: MockerFixture,
     safe_loop: AbstractEventLoop,
-    ip_version: Optional[IPVersion],
-    services: Optional[List[str]],
-    timeout: Union[int, float],
+    beacon_params: Dict[str, Any],
+    expected_services: Set[str],
 ) -> None:
     """Test beacon listener."""
 
     def _send_signal() -> None:
-        time.sleep(timeout + 0.5)
+        time.sleep(beacon_params["timeout"] + 0.5)
         os.kill(os.getpid(), signal.SIGINT)
 
     thread = threading.Thread(target=_send_signal, daemon=True)
@@ -46,12 +68,18 @@ def test_beacon_listener(
         pass
 
     listener = BeaconListener(
-        ip_version=ip_version,
         handlers=[_on_service_state_change],
-        services=services,
-        timeout=timeout,
+        **beacon_params,
     )
 
     with pytest.raises(KeyboardInterrupt):
         listener.run_forever()
+
+    assert expected_services.issubset(listener.services)
+
     listener.stop()
+
+    # asserting the loop is stopped but not closed
+    assert not safe_loop.is_running()
+    assert not safe_loop.is_closed()
+    safe_loop.close.assert_called_once_with()  # type: ignore
