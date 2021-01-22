615d8c39bea4e91bde525aa455f0d304aa623c4b
Federico Jaureguialzo <fedejaure@gmail.com>
2021-01-22 19:45:12 +0100

    Add Listen Layout

--- src/mdns_beacon/cli/main.py
+++ src/mdns_beacon/cli/main.py
@@ -1,28 +1,17 @@
 """Console script for mdns-beacon."""
 from ipaddress import IPv4Address, IPv6Address
-from typing import Any, Dict, Iterable, Union
+from typing import Iterable, Union
 
 import click
 from rich.console import Console
-from rich.table import Table
-from zeroconf import IPVersion, ServiceStateChange, Zeroconf
+from rich.live import Live
 
 from mdns_beacon import Beacon, BeaconListener, __version__
 from mdns_beacon.beacon import PROTOCOL
+from mdns_beacon.cli.layouts import ListenLayout
 from mdns_beacon.cli.types import IpAddress
 
 console = Console()
-
-mDNS_services: Dict[str, Any] = {}
-TABLE_SERVICES_COLUMNS = [
-    "#",
-    "Type",
-    "Name",
-    "Address IPv4",
-    "Port",
-    "Server",
-    "TTL",
-]
 
 
 @click.group()
@@ -79,51 +68,6 @@ def blink(
         beacon.stop()
 
 
-def print_services() -> None:
-    """Print services."""
-    console.clear()
-    table = Table()
-    table.width = console.width
-    table.title = (
-        ":police_car_light::satellite_antenna:"
-        " mDNS Beacon Listener "
-        ":satellite_antenna::police_car_light:"
-    )
-
-    for key in TABLE_SERVICES_COLUMNS:
-        table.add_column(key, no_wrap=True)
-
-    for index, service in enumerate(mDNS_services.values()):
-        table.add_row(
-            str(index), *[str(v) for k, v in service.items() if k in TABLE_SERVICES_COLUMNS]
-        )
-
-    console.print(table, justify="center")
-    console.print("Listen for services (Press CTRL+C to quit) ...")
-
-
-def on_service_state_change(
-    zeroconf: Zeroconf, service_type: str, name: str, state_change: ServiceStateChange
-) -> None:
-    """On service state change handler."""
-    global mDNS_services
-    service_id = f"{name}_{service_type}"
-    if state_change is ServiceStateChange.Removed:
-        mDNS_services.pop(service_id, None)
-    else:
-        info = zeroconf.get_service_info(service_type, name)
-        if info:
-            mDNS_services[service_id] = {
-                "Type": info.type,
-                "Name": info.name,
-                "Address IPv4": ",".join(info.parsed_addresses(IPVersion.V4Only)),
-                "Port": info.port,
-                "Server": info.server,
-                "TTL": info.host_ttl,
-            }
-    print_services()
-
-
 @main.command()
 @click.option(
     "--service",
@@ -134,14 +78,15 @@ def on_service_state_change(
 )
 def listen(services: Iterable[str]) -> None:
     """Listen for services on the local network."""
-    print_services()
-    listener = BeaconListener(services=list(services), handlers=[on_service_state_change])
-    try:
-        listener.run_forever()
-    except KeyboardInterrupt:
-        console.print("Shutting down ...")
-    finally:
-        listener.stop()
+    with Live(console=console, transient=True, auto_refresh=True) as live:
+        layout = ListenLayout(live=live)
+        listener = BeaconListener(services=list(services), handlers=[layout.update_services])
+        try:
+            listener.run_forever()
+        except KeyboardInterrupt:
+            console.print("Shutting down ...")
+        finally:
+            listener.stop()
 
 
 if __name__ == "__main__":
